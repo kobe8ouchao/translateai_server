b1ad4ffbe8fd35bfee2abee5242eba0b23af6142
Ouchao <ouchao>
2025-11-08 16:00:47 +0800

    x

--- pics.py
+++ pics.py
@@ -1,33 +1,3 @@
-# from openai import OpenAI
-# import base64
-
-
-# def encode_image(image_path):
-#     with open(image_path, "rb") as image_file:
-#         return base64.b64encode(image_file.read()).decode('utf-8')
-
-# base64_image = encode_image("./upload/weibo.jpg")
-
-# response = client.chat.completions.create(
-#   model="gpt-4o",
-#   messages=[
-#     {
-#       "role": "user",
-#       "content": [
-#         {"type": "text", "text": "Translate text to Simple Chinese?"},
-#         {
-#           "type": "image_url",
-#           "image_url": {
-#             "url": f"data:image/jpeg;base64,{base64_image}"
-#           },
-#         },
-#       ],
-#     }
-#   ],
-#   max_tokens=300,
-# )
-
-# print(response.choices[0])
 import cv2
 import numpy as np
 
